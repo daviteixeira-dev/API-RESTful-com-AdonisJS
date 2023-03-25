34fbc7e12027a9bf62e6bf6f20d40fc6c3d8986a
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-24 23:54:30 -0300

    Installing ORM LUCID, adding DB SQLite and Configuring CORS

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -20,6 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.get('/', async () => {
+    return { hello: 'world' }
+  })
+}).prefix('/api')
