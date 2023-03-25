289b615183599deb9b548efda0b686ba97327e99
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-25 20:10:00 -0300

    Inserting data into the database

--- backend/app/Controllers/Http/MomentsController.ts
+++ backend/app/Controllers/Http/MomentsController.ts
@@ -1,10 +1,21 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Moment from 'App/Models/Moment'
 
 export default class MomentsController {
 
-  public async store(){
+  public async store({request, response}: HttpContextContract){
+
+    const body = request.body()
+
+    const moment = await Moment.create(body)
+
+    response.status(201)
+
     return {
-      msg: 'Deu certo!',
+      message: 'Momento criado com sucesso!',
+      data: moment,
     }
+
   }
 }
