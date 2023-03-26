3e3c4d9860d672199802cf3c916180b149df189d
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-25 22:41:25 -0300

    effecting the relations

--- backend/app/Models/Moment.ts
+++ backend/app/Models/Moment.ts
@@ -1,7 +1,11 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Comment from './Comment'
 
 export default class Moment extends BaseModel {
+  @hasMany(() => Comment)
+  public comments: HasMany<typeof Comment>
+
   @column({ isPrimary: true })
   public id: number
 
--- backend/database/migrations/1679794294482_comments.ts
+++ backend/database/migrations/1679794294482_comments.ts
@@ -9,6 +9,8 @@ export default class extends BaseSchema {
       table.string("username")
       table.string("text")
 
+      table.integer("moment_id").unsigned().references("moments.id").onDelete("CASCADE")
+
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
