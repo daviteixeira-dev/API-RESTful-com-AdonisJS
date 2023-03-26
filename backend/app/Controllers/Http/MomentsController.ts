804c4bb921b734ddf5cfbcb9c81635da32eabd98
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-25 21:30:21 -0300

    image upload via API with AdonisJS

--- backend/app/Controllers/Http/MomentsController.ts
+++ backend/app/Controllers/Http/MomentsController.ts
@@ -1,12 +1,33 @@
+import { v4 as uuidv4 } from 'uuid'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Moment from 'App/Models/Moment'
 
+import Application from '@ioc:Adonis/Core/Application'
+
 export default class MomentsController {
+
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
 
   public async store({request, response}: HttpContextContract){
 
     const body = request.body()
+
+    const image = request.file('image', this.validationOptions)
+
+    if(image){
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName
+      })
+
+      body.image = imageName
+    }
 
     const moment = await Moment.create(body)
 
