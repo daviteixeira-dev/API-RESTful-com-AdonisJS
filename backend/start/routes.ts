69d3735bdb29ec1ee38c84755028eca74774be77
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-25 22:45:12 -0300

    creating a controller

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -26,4 +26,6 @@ Route.group(() => {
   })
 
   Route.resource("/moments", "MomentsController").apiOnly()
+
+  Route.post("/moments/:momentId/comments", "CommentsController.store")
 }).prefix('/api')
